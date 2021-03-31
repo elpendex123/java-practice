ba8d9d951b0c4e94475d95e1d8ada3ff32a10edd
Enrique Coello <enrique.coello@gmail.com>
2021-03-31 11:48:22 -0400

    Updated driver in Main class to initialize and compare 3 lists to test out methods in List class

--- src/com/coello/Main.java
+++ src/com/coello/Main.java
@@ -3,7 +3,42 @@ package com.coello;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("hello");
+        MyList l1 = new MyList("List #1");
+        l1.add(1);
+        l1.add(2);
+        l1.add(3);
+        System.out.println(l1);
+
+        MyList l2 = new MyList("List #1");
+        l2.add(1);
+        l2.add(2);
+        l2.add(3);
+        System.out.println(l2);
+
+        MyList l3 = new MyList("List #3");
+        l3.add(4);
+        l3.add(5);
+        l3.add(6);
+        System.out.println(l3);
+
+        System.out.println("\nComparing:");
+
+        System.out.println("\nComparing to self, should be true");
+        if (l1.equals(l1))
+            System.out.println("Same");
+        else
+            System.out.println("Different");
+
+        System.out.println("\nComparing to list 2, same values, should be true");
+        if (l1.equals(l2))
+            System.out.println("Same");
+        else
+            System.out.println("Different");
+
+        System.out.println("\nComparing to different name and values, should be false");
+        if (l1.equals(l3))
+            System.out.println("Same");
+        else
+            System.out.println("Different");
     }
 }
